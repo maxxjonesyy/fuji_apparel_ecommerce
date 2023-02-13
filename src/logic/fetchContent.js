141a066cb1914e8aa58820b7c35981f7c918de4a
Maxwell Jones <maxxjonesyy@gmail.com>
2023-02-13 17:40:34 +1100

    clean up contentful

--- src/logic/fetchContent.js
+++ src/logic/fetchContent.js
@@ -1,9 +1,6 @@
 import * as contentful from "contentful";
 
-const images = {};
-const products = {};
-
-const data = [];
+const content = { images: {}, products: {} };
 
 function fetchContent(type) {
   const client = contentful.createClient({
@@ -17,27 +14,28 @@ function fetchContent(type) {
     .catch((error) => console.error(error));
 }
 
-fetchContent("images").then((response) => {
-  response.forEach((item) => {
-    images[item.fields.name] = {
-      name: item.fields.name,
-      url: item.fields.image.fields.file.url,
-    };
-  });
-});
+function contentCleanup() {
+  fetchContent("images").then((response) =>
+    response.forEach((item) => {
+      content.images[item.fields.name] = {
+        name: item.fields.name,
+        src: item.fields.image.fields.file.url,
+      };
+    })
+  );
 
-fetchContent("product").then((response) => {
-  response.forEach((item) => {
-    products[item.fields.productName] = {
-      name: item.fields.productName,
-      images: item.fields.productImage,
-      description: item.fields.productDescription,
-      price: item.fields.productPrice,
-    };
-  });
-});
+  fetchContent("product").then((response) =>
+    response.forEach((item) => {
+      content.products[item.fields.name] = {
+        name: item.fields.name,
+        description: item.fields.productDescription,
+        images: item.fields.productImage,
+        price: item.fields.productPrice,
+      };
+    })
+  );
+}
 
-data.products = products;
-data.images = images;
+contentCleanup();
 
-export { data };
+export { content };
