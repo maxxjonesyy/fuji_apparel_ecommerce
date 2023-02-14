b209ad0c978c26e36bfeaeba2c920478d9b17a86
Maxwell Jones <maxxjonesyy@gmail.com>
2023-02-14 12:18:14 +1100

    config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {},
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+      },
+    },
   },
   plugins: [],
 };
