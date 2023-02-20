2f40c98687eb6245b0ecba14defb09cb50580e88
Maxwell Jones <maxxjonesyy@gmail.com>
2023-02-20 19:29:20 +1100

    import material ui dropdown & product component

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,33 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
+import { FormControl, Select, MenuItem, InputLabel } from "@mui/material";
 
 import Navbar from "../components/Navbar";
-import { useLocation } from "react-router-dom";
 
 function ProductPage() {
   const { state } = useLocation();
+  const [size, setSize] = useState("");
+
+  function handleSizeChange(event) {
+    setSize(event.target.value);
+  }
+
+  function handleCartSubmit() {
+    if (size === "") {
+      alert("Please select a size before proceeding");
+    }
+  }
 
   return (
-    <>
+    <div className='flex flex-col h-screen'>
       <Navbar />
 
-      <div className='grid items-center grid-cols-3'>
-        <div className='grid grid-cols-2 col-span-2'>
-          {state.product.images.map((image, index) => (
-            <img
-              key={index}
-              src={image.fields.file.url}
-              alt={image.fields.title}
-              className='w-full'
-            />
-          ))}
-        </div>
+      <div className='flex-1'>
+        <div className='flex flex-col items-center justify-center h-full 2xl:grid 2xl:grid-cols-3 '>
+          <div className='lg:grid lg:grid-cols-2 lg:col-span-2'>
+            {state.product.images.map((image, index) => (
+              <img
+                key={index}
+                src={image.fields.file.url}
+                alt={image.fields.title}
+                className='w-[500px] lg:w-full'
+              />
+            ))}
+          </div>
 
-        <div className=''>
-          <h1>{state.product.name}</h1>
-          <p>{state.product.description}</p>
+          <div className='flex flex-col items-center gap-10 px-5 py-10'>
+            <h1 className='pt-5 product-heading'>{state.product.name}</h1>
+            <span className='product-price'>${state.product.price}</span>
+            <p className='w-full lg:w-2/3'>{state.product.description}</p>
+            <FormControl className='w-full lg:w-2/3'>
+              <InputLabel>Select Size</InputLabel>
+              <Select
+                value={size}
+                label='Select Size'
+                onChange={handleSizeChange}
+              >
+                <MenuItem value='Small'>Small</MenuItem>
+                <MenuItem value='Medium'>Medium</MenuItem>
+                <MenuItem value='Large'>Large</MenuItem>
+                <MenuItem value='X Large'>X Large</MenuItem>
+              </Select>
+            </FormControl>
+            <button
+              className='w-full text-white bg-black rounded-md h-[50px] lg:w-2/3'
+              onClick={handleCartSubmit}
+            >
+              Add to Cart
+            </button>
+          </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
