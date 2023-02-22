6bc5fe268250585e6c97a4ef12ff126a4ba67189
Maxwell Jones <maxxjonesyy@gmail.com>
2023-02-22 14:24:52 +1100

    make navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 function Navbar() {
-  const cart = useSelector((state) => state.cart.value);
+  const cart = useSelector((state) => state.cart.cartState);
 
   function handleHamburger(toggled) {
     const hamburgerList = document.getElementById("hamburger_list");
@@ -20,43 +20,45 @@ function Navbar() {
 
   return (
     <>
-      <nav className='flex items-center justify-between p-3'>
+      <nav className='flex items-center justify-between p-3 lg:justify-around lg:p-1'>
         <div className='flex items-center'>
-          <img src='logo.svg' alt='logo' className='w-24' />
-          <h1 className='text-2xl font-bold uppercase '>Fuji Apparel</h1>
+          <img src='logo.svg' alt='logo' className='w-24 lg:w-48' />
+          <h1 className='text-2xl font-bold uppercase lg:text-4xl'>
+            Fuji Apparel
+          </h1>
         </div>
 
         <aside aria-label='Sidebar' id='hamburger' className='z-[10]'>
-          <div className='relative z-[10]'>
+          <div className='relative z-[10] lg:hidden'>
             <Hamburger onToggle={handleHamburger} size={30} />
           </div>
           <ul
             id='hamburger_list'
-            className='fixed w-[60%] h-screen top-0 right-0 bg-white translate-x-full'
+            className='fixed w-[60%] h-screen top-0 right-0 bg-white translate-x-full lg:relative lg:h-auto lg:w-auto lg:translate-x-0'
           >
-            <div className='flex flex-col justify-center h-full gap-3 p-3'>
-              <li className='p-1 text-xl rounded-md hover:bg-lightGrey'>
-                <Link to='/' className='pl-1'>
+            <div className='flex flex-col justify-center h-full gap-3 p-3 lg:flex-row lg:gap-10'>
+              <li className='nav-heading'>
+                <Link to='/' className='link-padding'>
                   Home
                 </Link>
               </li>
-              <li className='p-1 text-xl rounded-md hover:bg-lightGrey'>
-                <Link to='/about' className='pl-1'>
+              <li className='nav-heading'>
+                <Link to='/about' className='link-padding'>
                   About
                 </Link>
               </li>
-              <li className='p-1 text-xl rounded-md hover:bg-lightGrey'>
-                <Link to='/shop' className='pl-1'>
+              <li className='nav-heading'>
+                <Link to='/shop' className='link-padding'>
                   Shop
                 </Link>
               </li>
-              <li className='p-1 text-xl rounded-md hover:bg-lightGrey'>
-                <Link to='/contact' className='pl-1'>
+              <li className='nav-heading'>
+                <Link to='/contact' className='link-padding'>
                   Contact
                 </Link>
               </li>
-              <li className='p-1 text-xl rounded-md hover:bg-lightGrey'>
-                <Link to='/cart'>
+              <li className='nav-heading'>
+                <Link to='/cart' className='link-padding'>
                   Cart
                   <div className='inline-block w-8 ml-5 text-sm text-center bg-black rounded-sm'>
                     <span className='text-white'>{cart.length}</span>
